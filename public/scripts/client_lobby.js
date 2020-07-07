a0f87f1be5d3e7af383cfee7ad4ef0e4608de81d
vch9 <valentinchb@gmail.com>
2020-07-07 15:42:40 +0200

    Join lobby on connection

--- public/scripts/client_lobby.js
+++ public/scripts/client_lobby.js
@@ -4,3 +4,8 @@
 function fetchPlayers(socket, uuid) {
     socket.emit('fetch players', uuid);
 }
+
+function joinLobby(socket, uuid, pseudo) {
+    const msg = uuid + ';' + pseudo;
+    socket.emit('join lobby', msg)
+}
